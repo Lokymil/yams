8bceaebc642c4103971a10f3f4ddfa07ed27f0ec
Christophe ARRESTIER <arrestierchristophe@gmail.com>
2023-09-15 14:22:48 +0200

    Add yams figure

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-const getDiceThrowPoints = () => {
+const isYams = (dices) => dices.filter(dice => dice === dices[0]).length === 5;
+
+const getDiceThrowPoints = (dices) => {
+    if (!dices || dices.length === 0) {
+        return 0;
+    }
+
+    if (isYams(dices)) {
+        return 50;
+    }
+
     return 0;
 };
 
--- index.test.js
+++ index.test.js
@@ -4,4 +4,8 @@ describe("getDiceThrowPoints", () => {
     it('should give 0 point when no dices have been thrown', () => {
         expect(getDiceThrowPoints()).toBe(0)
     })
+
+    it('should give 50 points when a YAMS figure have been thrown', () => {
+        expect(getDiceThrowPoints([5, 5, 5, 5, 5])).toBe(50);
+    })
 })
