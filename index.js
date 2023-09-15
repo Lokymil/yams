b8eeae24b8345037ac3e29f9d3dbbdf669ba26dd
Christophe ARRESTIER <arrestierchristophe@gmail.com>
2023-09-15 14:37:34 +0200

    Add square figure

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
-const isYams = dices => dices.filter(dice => dice === dices[0]).length === 5;
+const isYams = (dices) =>
+  dices.filter((dice) => dice === dices[0]).length === 5;
 
-const isGreatSuite = dices => 
-    dices.sort().every((dice, index, sortedDices) => 
-        index === 0 ? true : dice - 1 === sortedDices[index - 1]
-    )
+const isGreatSuite = (dices) =>
+  dices
+    .sort()
+    .every((dice, index, sortedDices) =>
+      index === 0 ? true : dice - 1 === sortedDices[index - 1]
+    );
 
+const isSquare = (dices) =>
+  Object.values(
+    dices.reduce((scores, dice) => ({ ...scores, [dice]: scores[dice] + 1 }), {
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+      6: 0,
+    })
+  ).some((countOfDice) => countOfDice === 4);
 
 const getDiceThrowPoints = (dices) => {
-    if (!dices || dices.length === 0) {
-        return 0;
-    }
-
-    if (isYams(dices)) {
-        return 50;
-    }
-
-    if (isGreatSuite(dices)) {
-        return 40;
-    }
-
+  if (!dices || dices.length === 0) {
     return 0;
+  }
+
+  if (isYams(dices)) {
+    return 50;
+  }
+
+  if (isGreatSuite(dices)) {
+    return 40;
+  }
+
+  if (isSquare(dices)) {
+    return 35;
+  }
+
+  return 0;
 };
 
 module.exports = {
-    getDiceThrowPoints
+  getDiceThrowPoints,
 };
--- index.test.js
+++ index.test.js
@@ -1,19 +1,23 @@
-const { getDiceThrowPoints } = require(".")
+const { getDiceThrowPoints } = require(".");
 
 describe("getDiceThrowPoints", () => {
-    it('should give 0 point when no dices have been thrown', () => {
-        expect(getDiceThrowPoints()).toBe(0)
-    })
+  it("should give 0 point when no dices have been thrown", () => {
+    expect(getDiceThrowPoints()).toBe(0);
+  });
 
-    it('should give 50 points when a YAMS figure have been thrown', () => {
-        expect(getDiceThrowPoints([5, 5, 5, 5, 5])).toBe(50);
-    })
+  it("should give 50 points when a YAMS figure have been thrown", () => {
+    expect(getDiceThrowPoints([5, 5, 5, 5, 5])).toBe(50);
+  });
 
-    it('should give 40 points when a sorted great suite figure have been thrown', () => {
-        expect(getDiceThrowPoints([1, 2, 3, 4, 5])).toBe(40);
-    })
+  it("should give 40 points when a sorted great suite figure have been thrown", () => {
+    expect(getDiceThrowPoints([1, 2, 3, 4, 5])).toBe(40);
+  });
 
-    it('should give 40 points when a unsorted great suite figure have been thrown', () => {
-        expect(getDiceThrowPoints([5, 2, 4, 3, 6])).toBe(40);
-    })
-})
+  it("should give 40 points when a unsorted great suite figure have been thrown", () => {
+    expect(getDiceThrowPoints([5, 2, 4, 3, 6])).toBe(40);
+  });
+
+  it("should give 35 points when a square figure have been thrown", () => {
+    expect(getDiceThrowPoints([1, 4, 4, 4, 4])).toBe(35);
+  });
+});
